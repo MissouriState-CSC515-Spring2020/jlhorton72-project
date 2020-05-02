7a377ea97f3a8870105014e6df63823a8ed03200
Jim Horton <38473243+jlhorton72@users.noreply.github.com>
2020-05-01 22:10:56 -0500

    working chkpnt 6

--- jimsphotos/src/App.js
+++ jimsphotos/src/App.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React, {Suspense, lazy} from 'react';
 
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Posts from './Components/Posts/Posts';
-import VidInfo from './Components/VideoInfo/VideoInfo';
-import RecentVids from './Components/RecentVideos/RecentVideo';
+// import Posts from './Components/Posts/Posts';
+// import VidInfo from './Components/VideoInfo/VideoInfo';
+// import RecentVids from './Components/RecentVideos/RecentVideo';
 
-import Catrergories from './Components/Categories/Categories';
-import Recents from './Components/Recents/RecentPics';
-import DogCategory from './Components/DogCategory/DogCategory';
-import PictureDescription from './Components/Description/Description';
+const Posts = lazy(() => import('./Components/Posts/Posts'));
+const VidInfo = lazy(() => import('./Components/VideoInfo/VideoInfo'));
+const RecentVids = lazy(() => import('./Components/RecentVideos/RecentVideo'));
+
+
+// import Catrergories from './Components/Categories/Categories';
+// import Recents from './Components/Recents/RecentPics';
+// import DogCategory from './Components/DogCategory/DogCategory';
+// import PictureDescription from './Components/Description/Description';
 
 
 function App() {
   return (
     <Router>
+      <Suspense fallback ={<div>Loading..</div>}>
       <div className="App">
         
         <Switch>
@@ -27,6 +33,7 @@ function App() {
           <Route path = '/' exact component = {Posts}></Route>
         </Switch>
       </div>
+      </Suspense>
     </Router>
   );
 }
